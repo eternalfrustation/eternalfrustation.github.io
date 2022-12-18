fb7287b9236d012fbeb0efb3f3dfaa6645c0cfd7
eternalfrustation <eternalfrustation@users.noreply.github.com>
2022-12-18 06:45:31 +0000

    deploy: 23d7b3ca73a99f743228dc0af1d93f07461942a1

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+	["h1", "span", "span", "svg", ".button", ".post-entry", "h2", "p", "blockquote", ".profile img"].forEach((e, index) => {
+		setTimeout(
+		() => {document.querySelectorAll(e).forEach((i) => {
+			i.style.opacity = "1";
+			i.style.filter = "blur(0px)";
+			i.style.transform = "translateY(0rem)";
+		})}, index*400);	
+	})
