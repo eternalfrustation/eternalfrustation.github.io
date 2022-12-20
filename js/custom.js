0a862a76c93eaed186c01cb78863d1ed2e28426a
eternalfrustation <eternalfrustation@users.noreply.github.com>
2022-12-20 06:59:06 +0000

    deploy: 80f421bc43dbb28d6936b6f18e82997b20eed726

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,3 @@
-/*
-let body = document.getElementsByTagName("body")[0];
-const namespace = "http://www.w3.org/2000/svg";
-let svg = document.createElementNS(namespace, "svg");
-svg.setAttribute("width", "100%");
-svg.setAttribute("height", "100%");
-const rect = document.createElementNS(namespace, "rect");
-rect.setAttribute("x", "0");
-rect.setAttribute("y", "0");
-rect.setAttribute("width", "10vh");
-rect.setAttribute("height", "10vh");
-rect.setAttribute("fill", "red");
-svg.appendChild(rect);
-svg.style.display = "block";
-svg.style.position = "fixed";
-svg.style.top = "0px";
-body.appendChild(svg);
-*/
 ["h1", "span", "span", "svg", ".button", ".post-entry", "h2", "p", "blockquote", ".profile img"].forEach((e, index) => {
 	setTimeout(
 	() => {document.querySelectorAll(e).forEach((i) => {
