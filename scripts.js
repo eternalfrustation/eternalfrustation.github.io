57b3f915ed612d66749de74b0601620ad54f830f
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-04-02 04:33:39 +0530

    Redsigned it

---
+++ scripts.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+	const cards = document.querySelectorAll("body > div");
+	for (const card of cards) {
+		card.addEventListener("mouseover", (e) => {
+			const boundingBox = card.getBoundingClientRect();
+			const mrx =  (e.x - boundingBox.x) / boundingBox.width;
+			const mry =  (e.y - boundingBox.y) / boundingBox.height;
+			card.style.transform = `skew(${mrx}deg, ${mry}deg)`
+		}, true)
+		card.addEventListener("mouseleave", (e) => {
+			card.style.transform = ``;
+		})
+	}
+})
